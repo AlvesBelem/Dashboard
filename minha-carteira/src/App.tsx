7ebac249d52a33d323d256b7b7a7f802f3724f69
AlvesBelem <marcelocartoes.pa@gmail.com>
2022-04-13 19:56:38 -0300

    add styled-components

---
+++ minha-carteira/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Dashboard from './pages/Dashboard'
+import GlobalStyles from './styles/GlobalStyles';
+
+
+
+const App: React.FC = () => {
+    return (
+        <>
+            <GlobalStyles />
+            <Dashboard />
+        </>
+    );
+}
+
+export default App
+
